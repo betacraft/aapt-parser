88e6fc86e93c6daca000c01d081a00ceef2fa84a
Abhishek <abhishek.bhattacharjee11@gmail.com>
2016-02-03 11:51:43 +0530

    Changed test

--- parser_test.go
+++ parser_test.go
@@ -2,26 +2,35 @@ package aaptparse
 
 import (
 	"fmt"
+	"log"
 	"testing"
+	"time"
 )
 
 func TestParseApk(t *testing.T) {
-	apk := Parse("eventbrite.apk")
-
-	if apk == nil {
-		t.Fail()
+	for {
+		apk, err := Parse("fbwork.apk")
+		log.Println("error: ", err)
+		if err != nil {
+			log.Println("error: ", err)
+		}
+		if apk == nil {
+			t.Fail()
+		}
+		fmt.Println("Apk = ", "\n",
+			"Label:", apk.AppLabel, "\n",
+			"FeaturesNotRequired:", apk.FeaturesNotRequired, "\n",
+			"Gl Use: ", apk.GlUse, "\n",
+			"LibsNotRequired", apk.LibsNotRequired, "\n",
+			"PackageName: ", apk.PackageName, "\n",
+			"VersionCode: ", apk.VersionCode, "\n",
+			"VersionName:", apk.VersionName, "\n",
+			"SdkVersion:", apk.SdkVersion, "\n",
+			"TargetSdkVersion:", apk.TargetSdkVersion, "\n",
+			"UsesPermissions:", apk.Permissions, "\n",
+			"NativeCode:", apk.NativeCode, "\n",
+			"FeaturesRequired:", apk.FeaturesRequired)
+		fmt.Println("Waiting 2 secs...")
+		time.Sleep(2 * time.Second)
 	}
-	fmt.Println("Apk = ", "\n",
-		"Label:", apk.AppLabel, "\n",
-		"FeaturesNotRequired:", apk.FeaturesNotRequired, "\n",
-		"Gl Use: ", apk.GlUse, "\n",
-		"LibsNotRequired", apk.LibsNotRequired, "\n",
-		"PackageName: ", apk.PackageName, "\n",
-		"VersionCode: ", apk.VersionCode, "\n",
-		"VersionName:", apk.VersionName, "\n",
-		"SdkVersion:", apk.SdkVersion, "\n",
-		"TargetSdkVersion:", apk.TargetSdkVersion, "\n",
-		"UsesPermissions:", apk.Permissions, "\n",
-		"NativeCode:", apk.NativeCode, "\n",
-		"FeaturesRequired:", apk.FeaturesRequired)
 }
